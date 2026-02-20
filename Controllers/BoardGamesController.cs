45a446ba51af197360a00efbe2a7e0de50abaf9a
nathan <nguyendinhnhat206@gmail.com>
2026-02-20 16:26:54 -0500

    -- Initial commit board game list .NET 8 WebAPI setup

---
+++ Controllers/BoardGamesController.cs
@@ -0,0 +1,131 @@
+using System.Linq.Dynamic.Core;
+using System.Text.Json;
+using BoardGameList.Constants;
+using BoardGameList.DTO;
+using BoardGameList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace BoardGameList.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class BoardGamesController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    private readonly ILogger<BoardGamesController> _logger;
+    
+    private readonly IMemoryCache _memoryCache;
+
+    public BoardGamesController(ApplicationDbContext context, 
+        ILogger<BoardGamesController> logger,
+        IMemoryCache memoryCache)
+    {
+        _context = context;
+        _logger = logger;
+        _memoryCache = memoryCache;
+    }
+
+    [HttpGet(Name = "GetBoardGames")]
+    [ResponseCache(CacheProfileName = "Any-60")]
+    public async Task<RestDTO<BoardGame[]>> Get([FromQuery] RequestDTO<BoardGameDTO> input)
+    {
+        _logger.LogInformation(CustomLogEvents.BoardGamesController_Get, "Get Board Game List method started");
+        var query = _context.BoardGames.AsQueryable();
+        if (!string.IsNullOrEmpty(input.FilterQuery))
+            query = query.Where(b => b.Name.Contains(input.FilterQuery));
+        var recordCount = await query.CountAsync();
+        
+        BoardGame[]? result = null;
+        var cacheKey = $"{input.GetType()}-{JsonSerializer.Serialize(input)}";
+        if (!_memoryCache.TryGetValue<BoardGame[]>(cacheKey, out result))
+        {
+            query = query
+                .OrderBy($"{input.SortColumn} {input.SortOrder}")
+                .Skip(input.PageIndex * input.PageSize)
+                .Take(input.PageSize);
+            result = await query.ToArrayAsync();
+            _memoryCache.Set(cacheKey, result, new TimeSpan(0, 0, 30));
+        }
+
+        return new RestDTO<BoardGame[]>()
+        {
+            Data = result,
+            PageIndex = input.PageIndex,
+            PageSize = input.PageSize,
+            RecordCount = recordCount,
+            Links = new List<LinkDTO> {
+                new LinkDTO(Url.Action(null, "BoardGames", new { input.PageIndex, input.PageSize }, Request.Scheme)!,
+                    "self", 
+                    "GET"),
+            }
+        };
+    }
+    
+    [HttpPost(Name = "UpdateBoardGame")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    public async Task<RestDTO<BoardGame?>> Post(BoardGameDTO model)
+    {
+        var boardgame = await _context.BoardGames
+            .Where(b => b.Id == model.Id)
+            .FirstOrDefaultAsync();
+        if (boardgame != null)
+        {
+            if (!string.IsNullOrEmpty(model.Name))
+                boardgame.Name = model.Name;
+            if (model.Year.HasValue && model.Year.Value > 0)
+                boardgame.Year = model.Year.Value;
+            if (model.MinPlayers.HasValue && model.MinPlayers.Value > 0)
+                boardgame.MinPlayers = model.MinPlayers.Value;
+            if (model.MaxPlayers.HasValue && model.MaxPlayers.Value > 0)
+                boardgame.MaxPlayers = model.MaxPlayers.Value;
+            if (model.PlayTime.HasValue && model.PlayTime.Value > 0)
+                boardgame.PlayTime = model.PlayTime.Value;
+            if (model.MinAge.HasValue && model.MinAge.Value > 0)
+                boardgame.MinAge = model.MinAge.Value;
+            boardgame.LastModifiedDate = DateTime.Now;
+            _context.BoardGames.Update(boardgame);
+            await _context.SaveChangesAsync();
+        };
+
+        return new RestDTO<BoardGame?>()
+        {
+            Data = boardgame,
+            Links = new List<LinkDTO>
+            {
+                new LinkDTO(
+                    Url.Action(null, "BoardGames", model, Request.Scheme)!,
+                    "self",
+                    "POST"),
+            }
+        };
+    }
+    
+    [HttpDelete(Name = "DeleteBoardGame")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    public async Task<RestDTO<BoardGame?>> Delete(int id)
+    {
+        var boardgame = await _context.BoardGames
+            .Where(b => b.Id == id)
+            .FirstOrDefaultAsync();
+        if (boardgame != null)
+        {
+            _context.BoardGames.Remove(boardgame);
+            await _context.SaveChangesAsync();
+        };
+
+        return new RestDTO<BoardGame?>()
+        {
+            Data = boardgame,
+            Links = new List<LinkDTO>
+            {
+                new LinkDTO(
+                    Url.Action(null, "BoardGames", id, Request.Scheme)!,
+                    "self",
+                    "DELETE"),
+            }
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+using BoardGameList.Constants;
+using BoardGameList.Models;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Entity Framework Core DbContext configuration
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
+                       ?? throw new InvalidOperationException("Connection string 'DefaultConnection' for database book store not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+builder.Logging.ClearProviders().AddSimpleConsole().AddDebug();
+
+builder.Host.UseSerilog((ctx, lc) => {
+        lc.ReadFrom.Configuration(ctx.Configuration);
+        lc.Enrich.FromLogContext(); // Allows you to push custom properties
+        lc.WriteTo.File("Logs/log.txt",
+            outputTemplate:
+            "{Timestamp:HH:mm:ss} [{Level:u3}] " +
+            "[{MachineName} #{ThreadId}] " +
+            "{Message:lj}{NewLine}{Exception}",
+            rollingInterval: RollingInterval.Day);
+        lc.WriteTo.MySQL(connectionString, tableName: "LogEvents");
+    },
+    writeToProviders: true);
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(cfg => {
+        cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+        cfg.AllowAnyHeader();
+        cfg.AllowAnyMethod();
+    });
+    options.AddPolicy(name: "AnyOrigin",
+        cfg => {
+            cfg.AllowAnyOrigin();
+            cfg.AllowAnyHeader();
+            cfg.AllowAnyMethod();
+        });
+});
+
+builder.Services.AddControllers(options =>
+{
+    options.ModelBindingMessageProvider.SetValueIsInvalidAccessor(
+        (x) => $"The value '{x}' is invalid.");
+    options.ModelBindingMessageProvider.SetValueMustBeANumberAccessor(
+        (x) => $"The field {x} must be a number.");
+    options.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor(
+        (x, y) => $"The value '{x}' is not valid for {y}.");
+    options.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(
+        () => $"A value is required.");
+    
+    options.CacheProfiles.Add("NoCache", new CacheProfile() { NoStore = true });
+    options.CacheProfiles.Add("Any-60", new CacheProfile()
+        {
+            Location = ResponseCacheLocation.Any, 
+            Duration = 60
+        }
+    );
+});
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddResponseCaching(options =>
+{
+    options.MaximumBodySize = 32 * 1024 * 1024;
+    options.SizeLimit = 50 * 1024 * 1024;
+});
+
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (app.Configuration.GetValue<bool>("UseDeveloperExceptionPage"))
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/error");
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseResponseCaching();
+app.UseAuthorization();
+
+// app.Use((context, next) =>
+// {
+//     context.Response.GetTypedHeaders().CacheControl = 
+//         new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+//         {
+//             NoCache = true,
+//             NoStore = true
+//         };
+//     return next.Invoke();
+// });
+
+// Minimal API
+
+app.MapGet("/error",
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] (HttpContext context) =>
+    {
+        var exceptionHandler = context.Features.Get<IExceptionHandlerPathFeature>();
+        var details = new ProblemDetails();
+        details.Detail = exceptionHandler?.Error.Message;
+        details.Extensions["traceId"] = System.Diagnostics.Activity.Current?.Id ?? context.TraceIdentifier;
+        details.Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1";
+        details.Status = StatusCodes.Status500InternalServerError;
+        app.Logger.LogError(CustomLogEvents.Error_Get, exceptionHandler?.Error, "An unhandled exception occurred.");
+        return Results.Problem(details);
+    });
+
+app.MapGet("/error/test", 
+    [EnableCors("AnyOrigin")] 
+    [ResponseCache(NoStore = true)] () => { throw new Exception("test"); }
+    );
+
+app.MapGet("/cod/test",
+    [EnableCors("AnyOrigin")]
+    [ResponseCache(NoStore = true)] () =>
+        Results.Text("<script>" +
+                     "window.alert('Your client supports JavaScript!" +
+                     "\\r\\n\\r\\n" +
+                     $"Server time (UTC): {DateTime.UtcNow.ToString("o")}" +
+                     "\\r\\n" +
+                     "Client time (UTC): ' + new Date().toISOString());" +
+                     "</script>" +
+                     "<noscript>Your client does not support JavaScript</noscript>",
+            "text/html"));
+
+// Controllers
+app.MapControllers().RequireCors("AnyOrigin");
+
+app.Run();
